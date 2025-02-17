891f5ad44b752f4ae243c9f17fd2423cd325ebb5
mastermind-fa <falam3399@gmail.com>
2025-02-17 15:58:15 +0600

    active email

--- customers/views.py
+++ customers/views.py
@@ -35,7 +35,7 @@ class UserRegistrationApiView(APIView):
             print("token ", token)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
             print("uid ", uid)
-            confirm_link = f"https://foodie-delight-backend-eta.vercel.app/active/{uid}/{token}"
+            confirm_link = f"https://foodie-delight-backend-eta.vercel.app/customer/active/{uid}/{token}"
             email_subject = "Confirm Your Email"
             email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
             
